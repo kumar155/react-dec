8a034eb59509fc718c78b417f446dc516a42dc32
sadonthu <sadonthu@gmail.com>
2018-12-13 21:47:49 +0530

    latest

---
+++ src/components/firstComponent.js
@@ -0,0 +1,12 @@
+
+import React, {Component} from 'react';
+
+export class WelcomeComponent extends Component {
+    render() {
+        return(            
+            <h1>
+                This is my First react component..!
+            </h1>        
+        );
+    }
+}
